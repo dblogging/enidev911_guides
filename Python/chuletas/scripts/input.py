ae9f70fe17d34632e8d5e9800e3db1a7edea3591
enidev911 <enidev911@gmail.com>
2022-01-10 07:25:49 -0300

    up

--- Python/chuletas/scripts/input.py
+++ Python/chuletas/scripts/input.py
@@ -1,5 +1,27 @@
-val = input("Ingresa una expresión: ")
-print(type(val))
+import getpass
+ 
+#try:
+#    p = getpass.getpass('Contraseña: ')
+    
+#except Exception as error:
+#    print('ERROR', error)
+#else:
+#    print('Contraseña ingresada: ', p)
+
+#val = input("Ingresa una expresión: ")
+#print(type(val))
 
 #val = int(input("Ingresa un número: "))
 #print(val)
+
+ 
+user = getpass.getuser()
+ 
+while True:
+    pwd = getpass.getpass("User Name : %s " % user)
+ 
+    if pwd == 'abcd':
+        print("Welcome!!!")
+        break
+    else:
+        print("The password you entered is incorrect.")
